23e5e12961d3d3a28a3653ebd36d2225bdfe7297
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-31 17:08:31 -0500

    Moved error page into its own folder in template

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return render_template('404.html', error=e), 404
+    return render_template('error/404.html', error=e), 404
 
 @app.route('/', methods=("GET", "POST"))
 def search():
