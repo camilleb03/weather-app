bfc3868e6577b7ea916d020735d6c618ac2f19ee
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2021-01-01 17:56:32 -0500

    Changed file name and added test route for cities in circle

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, redirect, render_template, request, url_for, flash, abort
-from api import get_weather_by_city
+from api import get_weather_by_city, get_weather_by_cities_in_circle
 
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -30,8 +31,19 @@ def result(city_name):
     res = get_weather_by_city(city=city_name)
     if res.ok:
         data = res.json()
+        print("Response\n", data)
+        return render_template('weather/result.html', data=data)
+    else:
+        print(res.json()['message'])
+        abort(404, description=res.json()['message'])
+
+@app.route('/test')
+def test():
+    res = get_weather_by_cities_in_circle()
+    if res.ok:
+        data = res.json()
         print("Response\n", res)
-        return render_template('weather/result.html', city_name=city_name, data=data)
+        return render_template('weather/test.html', data=data)
     else:
         print(res.json()['message'])
         abort(404, description=res.json()['message'])
