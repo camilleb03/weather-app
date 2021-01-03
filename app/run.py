05e3a08dda1aa4683bbe91df4c2b4b3826e74293
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2021-01-03 16:57:11 -0500

    Added about route and link from nav to reach it

--- app/run.py
+++ app/run.py
@@ -126,5 +126,9 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
